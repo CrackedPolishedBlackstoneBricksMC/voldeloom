7d7a5f790f51f36f87845894493c65d3398e4317
quat1024 <quat1024@users.noreply.github.com>
2023-02-17 00:31:35 -0500

    Make the plugin 15 seconds faster with this ONE WEIRD TRICK
    yeah that was my cue to take a heap dump

--- src/main/java/net/fabricmc/loom/LoomGradlePlugin.java
+++ src/main/java/net/fabricmc/loom/LoomGradlePlugin.java
@@ -467,13 +467,6 @@ public class LoomGradlePlugin implements Plugin<Project> {
 				}
 			}
 		}
-		
-		try {
-			project.getLogger().lifecycle("SLEEPY TIME !!!!!!!!!!");
-			Thread.sleep(15000);
-		} catch (Throwable e) {
-			throw new RuntimeException(e);
-		}
 	}
 	
 	/**
