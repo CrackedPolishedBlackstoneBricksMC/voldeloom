4db1ace2bb88e8fd4eca080326bd38917d4a13f4
quat1024 <quat1024@users.noreply.github.com>
2023-02-21 01:28:11 -0500

    Fork Stitch to avoid adding duplicate SideOnly annotations where a forge binpatch already added one

--- src/main/java/net/fabricmc/loom/newprovider/Merger.java
+++ src/main/java/net/fabricmc/loom/newprovider/Merger.java
@@ -2,28 +2,12 @@ package net.fabricmc.loom.newprovider;
 
 import com.google.common.base.Preconditions;
 import net.fabricmc.loom.LoomGradleExtension;
-import net.fabricmc.stitch.merge.JarMerger;
+import net.fabricmc.loom.yoinked.stitch.ClassMergerCooler;
+import net.fabricmc.loom.yoinked.stitch.JarMergerCooler;
 import org.gradle.api.Project;
-import org.objectweb.asm.AnnotationVisitor;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.FieldVisitor;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Collections;
 
 /**
  * Merges two jars, such as a Minecraft client jar and server jar.
@@ -73,121 +57,20 @@ public class Merger extends NewProvider<Merger> {
 		if(Files.notExists(merged)) {
 			Files.createDirectories(merged.getParent());
 			
-			Path mergedUnfixed = Files.createTempFile(merged.getFileName().toString() + "-unfixed", ".jar.tmp");
+			log.lifecycle("|-> Target does not exist. Merging with JarMergerCooler to {}", merged);
 			
-			log.lifecycle("|-> Target does not exist. Merging with FabricMC JarMerger to {}", mergedUnfixed);
-			
-			try(JarMerger jm = new JarMerger(clientJar.toFile(), serverJar.toFile(), mergedUnfixed.toFile())) {
+			try(JarMergerCooler jm = new JarMergerCooler(clientJar.toFile(), serverJar.toFile(), merged.toFile())) {
+				jm.classMergerCooler = new ClassMergerCooler()
+					.sideEnum("Lcpw/mods/fml/relauncher/Side;")
+					.sideDescriptorAnnotation("Lcpw/mods/fml/relauncher/SideOnly;");
+				
 				jm.enableSyntheticParamsOffset();
 				jm.merge();
 			}
 			
-			log.lifecycle("|-> Remapping annotations...");
-			
-			try(FileSystem unfixedFs = FileSystems.newFileSystem(URI.create("jar:" + mergedUnfixed.toUri()), Collections.emptyMap());
-			    FileSystem fixedFs = FileSystems.newFileSystem(URI.create("jar:" + merged.toUri()), Collections.singletonMap("create", "true"))) {
-				Files.walkFileTree(unfixedFs.getPath("/"), new SimpleFileVisitor<Path>() {
-					@Override
-					public FileVisitResult preVisitDirectory(Path unfixedDir, BasicFileAttributes attrs) throws IOException {
-						Path fixedDir = fixedFs.getPath(unfixedDir.toString());
-						Files.createDirectories(fixedDir);
-						return FileVisitResult.CONTINUE;
-					}
-					
-					@Override
-					public FileVisitResult visitFile(Path unfixedFile, BasicFileAttributes attrs) throws IOException {
-						Path fixedFile = fixedFs.getPath(unfixedFile.toString());
-						if(unfixedFile.toString().endsWith(".class")) {
-							try(InputStream unfixedReader = new BufferedInputStream(Files.newInputStream(unfixedFile))) {
-								ClassReader unfixedClassReader = new ClassReader(unfixedReader);
-								ClassWriter fixedClassWriter = new ClassWriter(0);
-								unfixedClassReader.accept(new EnvironmentToSideOnlyVisitor(fixedClassWriter), 0);
-								Files.write(fixedFile, fixedClassWriter.toByteArray());
-							}
-						} else {
-							Files.copy(unfixedFile, fixedFile);
-						}
-						
-						return FileVisitResult.CONTINUE;
-					}
-				});
-			}
-			
-			log.lifecycle("|-> Annotation remap success.");
+			log.lifecycle("|-> Merged.");
 		}
 		
 		return this;
-	}
-	
-	//I deleted this class but pasted it back from upstream Voldeloom, so, this is all stuff figured out by TwilightFlower. Thanks!
-	private static class EnvironmentToSideOnlyVisitor extends ClassVisitor implements Opcodes {
-		private static final String FABRIC_ENVIRONMENT = "Lnet/fabricmc/api/Environment;";
-		private static final String FORGE_SIDEONLY = "Lcpw/mods/fml/relauncher/SideOnly;";
-		
-		private static final String FABRIC_ENVTYPE = "Lnet/fabricmc/api/EnvType;";
-		private static final String FORGE_SIDE = "Lcpw/mods/fml/relauncher/Side;";
-		
-		public EnvironmentToSideOnlyVisitor(ClassVisitor classVisitor) {
-			super(ASM4, classVisitor);
-		}
-		
-		public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
-			if(descriptor.equals(FABRIC_ENVIRONMENT)) {
-				return new EnvFixAnnotationVisitor(visitAnnotation(FORGE_SIDEONLY, visible));
-			} else {
-				return super.visitAnnotation(descriptor, visible);
-			}
-		}
-		
-		public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-			return new EnvFixMethodVisitor(super.visitMethod(access, name, descriptor, signature, exceptions));
-		}
-		
-		public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
-			return new EnvFixFieldVisitor(super.visitField(access, name, descriptor, signature, value));
-		}
-		
-		private static class EnvFixAnnotationVisitor extends AnnotationVisitor {
-			public EnvFixAnnotationVisitor(AnnotationVisitor annotationVisitor) {
-				super(ASM4, annotationVisitor);
-			}
-			
-			public void visitEnum(String name, String descriptor, String value) {
-				// both forge and fabric use SERVER and CLIENT so no value change necessary
-				if(descriptor.equals(FABRIC_ENVTYPE)) {
-					super.visitEnum(name, FORGE_SIDE, value); 
-				} else {
-					super.visitEnum(name, descriptor, value);
-				}
-			}
-		}
-		
-		private static class EnvFixMethodVisitor extends MethodVisitor {
-			public EnvFixMethodVisitor(MethodVisitor methodVisitor) {
-				super(Opcodes.ASM7, methodVisitor);
-			}
-			
-			public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
-				if(descriptor.equals(FABRIC_ENVIRONMENT)) {
-					return new EnvFixAnnotationVisitor(visitAnnotation(FORGE_SIDEONLY, visible));
-				} else {
-					return super.visitAnnotation(descriptor, visible);
-				}
-			}
-		}
-		
-		private static class EnvFixFieldVisitor extends FieldVisitor {
-			public EnvFixFieldVisitor(FieldVisitor fieldVisitor) {
-				super(Opcodes.ASM7, fieldVisitor);
-			}
-			
-			public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
-				if(descriptor.equals(FABRIC_ENVIRONMENT)) {
-					return new EnvFixAnnotationVisitor(visitAnnotation(FORGE_SIDEONLY, visible));
-				} else {
-					return super.visitAnnotation(descriptor, visible);
-				}
-			}
-		}
 	}
 }
---
+++ src/main/java/net/fabricmc/loom/yoinked/stitch/ClassMergerCooler.java
@@ -0,0 +1,263 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.loom.yoinked.stitch;
+
+import net.fabricmc.stitch.util.StitchUtil;
+import org.objectweb.asm.*;
+import org.objectweb.asm.tree.*;
+
+import java.util.*;
+
+/**
+ * This is a modified copy of Stitch's ClassMerger.
+ *
+ * Mainly I modified it to allow changing the annotatation types JarMerger adds to the jars.
+ */
+public class ClassMergerCooler {
+	private String sideEnum = "Lnet/fabricmc/api/EnvType;";
+	private String sidedInterfaceAnnotation = "Lnet/fabricmc/api/EnvironmentInterface;";
+	private String sidedInterfaceListAnnotation = "Lnet/fabricmc/api/EnvironmentInterfaces;";
+	private String sideDescriptorAnnotation = "Lnet/fabricmc/api/Environment;";
+	
+	public ClassMergerCooler sideEnum(String sideEnum) {
+		this.sideEnum = sideEnum;
+		return this;
+	}
+	
+	public ClassMergerCooler sidedInterfaceAnnotation(String sidedInterfaceAnnotation) {
+		this.sidedInterfaceAnnotation = sidedInterfaceAnnotation;
+		return this;
+	}
+	
+	public ClassMergerCooler sidedInterfaceListAnnotation(String sidedInterfaceListAnnotation) {
+		this.sidedInterfaceListAnnotation = sidedInterfaceListAnnotation;
+		return this;
+	}
+	
+	public ClassMergerCooler sideDescriptorAnnotation(String sideDescriptorAnnotation) {
+		this.sideDescriptorAnnotation = sideDescriptorAnnotation;
+		return this;
+	}
+	
+	private abstract static class Merger<T> {
+		private final Map<String, T> entriesClient, entriesServer;
+		private final List<String> entryNames;
+		
+		public Merger(List<T> entriesClient, List<T> entriesServer) {
+			this.entriesClient = new LinkedHashMap<>();
+			this.entriesServer = new LinkedHashMap<>();
+			
+			List<String> listClient = toMap(entriesClient, this.entriesClient);
+			List<String> listServer = toMap(entriesServer, this.entriesServer);
+			
+			this.entryNames = StitchUtil.mergePreserveOrder(listClient, listServer);
+		}
+		
+		public abstract String getName(T entry);
+		public abstract void applySide(T entry, String side);
+		
+		private List<String> toMap(List<T> entries, Map<String, T> map) {
+			List<String> list = new ArrayList<>(entries.size());
+			for (T entry : entries) {
+				String name = getName(entry);
+				map.put(name, entry);
+				list.add(name);
+			}
+			return list;
+		}
+		
+		public void merge(List<T> list) {
+			for (String s : entryNames) {
+				T entryClient = entriesClient.get(s);
+				T entryServer = entriesServer.get(s);
+				
+				if (entryClient != null && entryServer != null) {
+					list.add(entryClient);
+				} else if (entryClient != null) {
+					applySide(entryClient, "CLIENT");
+					list.add(entryClient);
+				} else {
+					applySide(entryServer, "SERVER");
+					list.add(entryServer);
+				}
+			}
+		}
+	}
+	
+	private void visitSideAnnotation(AnnotationVisitor av, String side) {
+		av.visitEnum("value", sideEnum, side.toUpperCase(Locale.ROOT));
+		av.visitEnd();
+	}
+	
+	private void visitItfAnnotation(AnnotationVisitor av, String side, List<String> itfDescriptors) {
+		for (String itf : itfDescriptors) {
+			AnnotationVisitor avItf = av.visitAnnotation(null, sidedInterfaceAnnotation);
+			avItf.visitEnum("value", sideEnum, side.toUpperCase(Locale.ROOT));
+			avItf.visit("itf", Type.getType("L" + itf + ";"));
+			avItf.visitEnd();
+		}
+	}
+	
+	public class SidedClassVisitor extends ClassVisitor {
+		private final String side;
+		private boolean alreadyHasOne = false;
+		
+		public SidedClassVisitor(int api, ClassVisitor cv, String side) {
+			super(api, cv);
+			this.side = side;
+		}
+		
+		@Override
+		public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
+			//avoid adding duplicate annotations where a forge binpatch already added one
+			if(descriptor.equals(sideDescriptorAnnotation)) alreadyHasOne = true;
+			
+			return super.visitAnnotation(descriptor, visible);
+		}
+		
+		@Override
+		public void visitEnd() {
+			if(!alreadyHasOne) {
+				AnnotationVisitor av = cv.visitAnnotation(sideDescriptorAnnotation, true);
+				visitSideAnnotation(av, side);
+			}
+			super.visitEnd();
+		}
+	}
+	
+	public byte[] merge(byte[] classClient, byte[] classServer) {
+		ClassReader readerC = new ClassReader(classClient);
+		ClassReader readerS = new ClassReader(classServer);
+		ClassWriter writer = new ClassWriter(0);
+		
+		ClassNode nodeC = new ClassNode(StitchUtil.ASM_VERSION);
+		readerC.accept(nodeC, 0);
+		
+		ClassNode nodeS = new ClassNode(StitchUtil.ASM_VERSION);
+		readerS.accept(nodeS, 0);
+		
+		ClassNode nodeOut = new ClassNode(StitchUtil.ASM_VERSION);
+		nodeOut.version = nodeC.version;
+		nodeOut.access = nodeC.access;
+		nodeOut.name = nodeC.name;
+		nodeOut.signature = nodeC.signature;
+		nodeOut.superName = nodeC.superName;
+		nodeOut.sourceFile = nodeC.sourceFile;
+		nodeOut.sourceDebug = nodeC.sourceDebug;
+		nodeOut.outerClass = nodeC.outerClass;
+		nodeOut.outerMethod = nodeC.outerMethod;
+		nodeOut.outerMethodDesc = nodeC.outerMethodDesc;
+		nodeOut.module = nodeC.module;
+		nodeOut.nestHostClass = nodeC.nestHostClass;
+		nodeOut.nestMembers = nodeC.nestMembers;
+		nodeOut.attrs = nodeC.attrs;
+		
+		if (nodeC.invisibleAnnotations != null) {
+			nodeOut.invisibleAnnotations = new ArrayList<>();
+			nodeOut.invisibleAnnotations.addAll(nodeC.invisibleAnnotations);
+		}
+		if (nodeC.invisibleTypeAnnotations != null) {
+			nodeOut.invisibleTypeAnnotations = new ArrayList<>();
+			nodeOut.invisibleTypeAnnotations.addAll(nodeC.invisibleTypeAnnotations);
+		}
+		if (nodeC.visibleAnnotations != null) {
+			nodeOut.visibleAnnotations = new ArrayList<>();
+			nodeOut.visibleAnnotations.addAll(nodeC.visibleAnnotations);
+		}
+		if (nodeC.visibleTypeAnnotations != null) {
+			nodeOut.visibleTypeAnnotations = new ArrayList<>();
+			nodeOut.visibleTypeAnnotations.addAll(nodeC.visibleTypeAnnotations);
+		}
+		
+		List<String> itfs = StitchUtil.mergePreserveOrder(nodeC.interfaces, nodeS.interfaces);
+		nodeOut.interfaces = new ArrayList<>();
+		
+		List<String> clientItfs = new ArrayList<>();
+		List<String> serverItfs = new ArrayList<>();
+		
+		for (String s : itfs) {
+			boolean nc = nodeC.interfaces.contains(s);
+			boolean ns = nodeS.interfaces.contains(s);
+			nodeOut.interfaces.add(s);
+			if (nc && !ns) {
+				clientItfs.add(s);
+			} else if (ns && !nc) {
+				serverItfs.add(s);
+			}
+		}
+		
+		if (!clientItfs.isEmpty() || !serverItfs.isEmpty()) {
+			AnnotationVisitor envInterfaces = nodeOut.visitAnnotation(sidedInterfaceListAnnotation, false);
+			AnnotationVisitor eiArray = envInterfaces.visitArray("value");
+			
+			if (!clientItfs.isEmpty()) {
+				visitItfAnnotation(eiArray, "CLIENT", clientItfs);
+			}
+			if (!serverItfs.isEmpty()) {
+				visitItfAnnotation(eiArray, "SERVER", serverItfs);
+			}
+			eiArray.visitEnd();
+			envInterfaces.visitEnd();
+		}
+		
+		new Merger<InnerClassNode>(nodeC.innerClasses, nodeS.innerClasses) {
+			@Override
+			public String getName(InnerClassNode entry) {
+				return entry.name;
+			}
+			
+			@Override
+			public void applySide(InnerClassNode entry, String side) {
+			}
+		}.merge(nodeOut.innerClasses);
+		
+		new Merger<FieldNode>(nodeC.fields, nodeS.fields) {
+			@Override
+			public String getName(FieldNode entry) {
+				return entry.name + ";;" + entry.desc;
+			}
+			
+			@Override
+			public void applySide(FieldNode entry, String side) {
+				//avoid adding duplicate annotations where a forge binpatch already added one
+				if(entry.visibleAnnotations == null || entry.visibleAnnotations.stream().noneMatch(a -> a.desc.equals(sideDescriptorAnnotation))) {
+					AnnotationVisitor av = entry.visitAnnotation(sideDescriptorAnnotation, false);
+					visitSideAnnotation(av, side);
+				}
+			}
+		}.merge(nodeOut.fields);
+		
+		new Merger<MethodNode>(nodeC.methods, nodeS.methods) {
+			@Override
+			public String getName(MethodNode entry) {
+				return entry.name + entry.desc;
+			}
+			
+			@Override
+			public void applySide(MethodNode entry, String side) {
+				//avoid adding duplicate annotations where a forge binpatch already added one
+				if(entry.visibleAnnotations == null || entry.visibleAnnotations.stream().noneMatch(a -> a.desc.equals(sideDescriptorAnnotation))) {
+					AnnotationVisitor av = entry.visitAnnotation(sideDescriptorAnnotation, false);
+					visitSideAnnotation(av, side);
+				}
+			}
+		}.merge(nodeOut.methods);
+		
+		nodeOut.accept(writer);
+		return writer.toByteArray();
+	}
+}
